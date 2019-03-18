7b5f4f6027894ba8d3fd6675f62d5c83065d2201
shirin <shirinanlen@gmail.com>
2019-03-18 13:30:34 -0400

    server routes and auth

---
+++ routes/auth.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SALT = 11;
+const SECRET = 'there are no jobs anyway';
+
+const hash = async (password) => {
+  return await bcrypt.hash(passord, SALT)
+};
+
+const encode = async (data) => {
+  return jwt.sign(data, SECRET)
+};
+
+const compare = async (passord) => {
+  const isAuthenticated = await bcrypt.compare(passord, user.password_digest)
+  return isAuthenticated
+};
+
+const verify = async (token) => {
+  return jwt.verify(token, SECRET)
+};
+
+const restrict = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(" ")[1];
+    const data = verify(token);
+    res.locals.user = data;
+    next();
+  } catch (e) {
+    console.log(e);
+    res.status(401).send('Invalid Creds')
+  }
+}
+
+module.exports = {
+  hash,
+  encode,
+  compare,
+  verify,
+  restrict
+};
