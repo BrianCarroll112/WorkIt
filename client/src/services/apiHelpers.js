052b6ff5756352126b2bc11bdb2545e5ac6ffadf
Brian <briancarroll112@gmail.com>
2019-03-18 16:03:41 -0400

    Update apiHelpers.js

--- client/src/services/apiHelpers.js
+++ client/src/services/apiHelpers.js
@@ -1,10 +1,28 @@
-import axios from 'axios';
+const axios = require('axios');
 const BASE_URL = 'http://localhost:3001';
 
 const registerUser = async (data) => {
-  const resp = await axios.post(`${BASE_URL}/user`, data);
+  const { first_name, last_name, email, password } = data;
+  const resp = await axios.post(`${BASE_URL}/user`, {
+    first_name,
+    last_name,
+    email,
+    password
+  });
 
   return resp.data
 };
 
-export { registerUser }
+const loginUser = async (email, password) => {
+  const resp = await axios.post(`${BASE_URL}/user/login`, {
+    email,
+    password
+  });
+  console.log(resp.data);
+  return resp.data;
+}
+
+export {
+  registerUser,
+  loginUser
+}
