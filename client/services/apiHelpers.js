3096e74392c04d8f585aa85b4b82d3624c824e34
Brian Carroll <briancarroll112@gmail.com>
2019-03-18 14:45:36 -0400

    apiHelper register

--- client/services/apiHelpers.js
+++ client/services/apiHelpers.js
@@ -1,5 +1,17 @@
 const axios = require('axios');
+const BASE_URL = 'http://localhost:3001';
 
-const registerUser = async (id, ) => {
+const registerUser = async (first_name, last_name, email, password ) => {
+  const resp = await axios.post(`${BASE_URL}/user`, {
+    first_name,
+    last_name,
+    email,
+    password
+  });
 
+  return resp.data
 };
+
+module.exports = {
+  registerUser,
+}
