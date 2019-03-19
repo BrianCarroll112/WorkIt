2feb891063d3226da0d3ee387017852eff5d6f68
alfonsodumlao <alfonso.dumlao@gmail.com>
2019-03-19 13:00:05 -0400

    added toggle function for jobtitle

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,6 @@ class App extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleRegister = this.handleRegister.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
-
   }
 
   handleChange(e) {
