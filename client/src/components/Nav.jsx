df3a54e7f16ba5b86b483ae678ad8acce1230e3d
shirin <shirinanlen@gmail.com>
2019-03-21 11:56:43 -0400

    design nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default (props) => (
-  <nav>
-    <Link to ="/jobs">Job List</Link>
-    <Link to ={`/user/${props.id}`}>Profile</Link>
+  <nav className='nav'>
+    <h2 className='logo'>WORK</h2>
+    <div className="links">
+    <Link className="jobsLink" to ="/jobs">Job List</Link>
+    <Link className="userLink" to ={`/user/${props.id}`}>Profile</Link>
+    </div>
   </nav>
 )
