a6d5ae8c6274bb7931893a48b72d40c60ec8ae73
Brian Carroll <briancarroll112@gmail.com>
2019-03-21 21:18:40 -0400

    placeholder pdf

--- client/src/components/Cv.jsx
+++ client/src/components/Cv.jsx
@@ -38,7 +38,7 @@ class Cv extends Component {
   render(){
     return(
     <div>
-    <embed src={this.props.cv ? this.props.cv : this.state.filepath.base64} width="400" height="300"/>
+    <embed src={this.state.filepath.base64 ? this.state.filepath.base64 : this.props.cv } width="400" height="300"/>
     <form>
       <FilesBase64 multiple={false} onDone={this.getFiles.bind(this)} />
       <button type='submit' onClick={this.handleUpload}> upload </button>
